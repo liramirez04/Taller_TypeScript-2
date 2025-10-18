6a5de7e6ff0b8783a8cc13b253c653929afae8ac
liramirez04 <li.ramirez@uniandes.edu.co>
2025-10-18 17:46:55 -0500

    Terminado

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,13 @@
 import { series } from './data.js';
 var seriesTable = document.getElementById("tablaSeries");
+var serieCard = document.getElementById("serieCard");
 mostrarSeries(series);
 function mostrarSeries(program) {
     var seriesTbody = document.createElement("tbody");
     for (var _i = 0, program_1 = program; _i < program_1.length; _i++) {
         var serie = program_1[_i];
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td class=\"table-active\">".concat(serie.id, "</td>\n        <td class=\"table-active\"> <a href=\"\" target=\"_blank\"> ").concat(serie.name, " </a></td>\n        <td class=\"table-active\">").concat(serie.channel, "</td>\n        <td class=\"table-active\">").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<td class=\"table-active\">".concat(serie.id, "</td>\n        <td class=\"table-active\"> <a class=\"link text-primary\" id=\"Card").concat(serie.id, "\"> ").concat(serie.name, " </a></td>\n        <td class=\"table-active\">").concat(serie.channel, "</td>\n        <td class=\"table-active\">").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     }
     seriesTable.appendChild(seriesTbody);
@@ -24,4 +25,25 @@ function temporadasPromedio(program) {
     }
     promedio = Math.floor(promedio / program.length);
     return promedio;
+}
+var links = document.querySelectorAll(".link");
+links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        serieCard.classList.remove('hidden');
+        var clickedLink = event.target.id;
+        var id_serie = Number(clickedLink.slice(4));
+        mostrarInfo(series[id_serie - 1]);
+    });
+});
+function mostrarInfo(serie) {
+    var seriesTbody = document.querySelector("#serieCard div");
+    if (seriesTbody) {
+        seriesTbody.innerHTML = "";
+    }
+    else {
+        seriesTbody = document.createElement("div");
+    }
+    seriesTbody.innerHTML = "<div class=\"card\">\n        <img class=\"card-img-top\" src=\"".concat(serie.image, "\" alt=\"Card image cap\">\n        <div class=\"card-body\">\n            <h5 class=\"card-title\">").concat(serie.name, "</h5>\n            <p class=\"card-text\">").concat(serie.description, "</p>\n            <a href=\"").concat(serie.link, "\" target=\"_blank\" class=\"card-link\">").concat(serie.link, "</a>\n        </div>\n    </div>");
+    serieCard.appendChild(seriesTbody);
 }
--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import {series} from './data.js'
 
 
 let seriesTable: HTMLElement = document.getElementById("tablaSeries")!;
+let serieCard: HTMLElement = document.getElementById("serieCard")!;
 
 mostrarSeries(series);
 
@@ -12,7 +13,7 @@ function mostrarSeries(program:Serie[]):void{
     {
         let trElement:HTMLElement = document.createElement("tr");
         trElement.innerHTML = `<td class="table-active">${serie.id}</td>
-        <td class="table-active"> <a href="" target="_blank"> ${serie.name} </a></td>
+        <td class="table-active"> <a class="link text-primary" id="Card${serie.id}"> ${serie.name} </a></td>
         <td class="table-active">${serie.channel}</td>
         <td class="table-active">${serie.seasons}</td>`;
         seriesTbody.appendChild(trElement);
@@ -34,4 +35,34 @@ function temporadasPromedio(program:Serie[]):number{
     }
     promedio=Math.floor(promedio/program.length);
     return promedio;
+}
+
+let links: NodeListOf<Element> = document.querySelectorAll(".link");
+
+links.forEach(link => {
+    link.addEventListener("click", function(event){
+        event.preventDefault();
+        serieCard.classList.remove('hidden');
+        let clickedLink = (event.target as HTMLElement).id;
+        let id_serie: number = Number(clickedLink.slice(4));
+        mostrarInfo(series[id_serie - 1]);
+    });
+});
+
+function mostrarInfo(serie:Serie):void{
+    let seriesTbody:HTMLElement = document.querySelector("#serieCard div")!;
+    if (seriesTbody){
+        seriesTbody.innerHTML = "";
+    } else {
+        seriesTbody = document.createElement("div");
+    }
+    seriesTbody.innerHTML = `<div class="card">
+        <img class="card-img-top" src="${serie.image}" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">${serie.name}</h5>
+            <p class="card-text">${serie.description}</p>
+            <a href="${serie.link}" target="_blank" class="card-link">${serie.link}</a>
+        </div>
+    </div>`;
+    serieCard.appendChild(seriesTbody);
 }
